959b0781eb1ed47d6d586dd1db09414bf72b4e65
emrah karaman <ekaraman89@hotmail.com>
2020-05-24 00:41:44 +0300

    NHibernate implementasyonu yapıldı

---
+++ Commerce.Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,24 @@
+﻿using NHibernate;
+using System;
+
+namespace Commerce.Core.DataAccess.NHibernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+        private static ISessionFactory _sessionFactory;
+
+        public virtual ISessionFactory SessionFactory => _sessionFactory ?? (_sessionFactory = InitializeFactory());
+
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return _sessionFactory.OpenSession();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Commerce.Core/DataAccess/NHibernate/NHibernateRepositoryBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Commerce.Core.Entities;
+
+namespace Commerce.Core.DataAccess.NHibernate
+{
+    class NHibernateRepositoryBase<TEntity> : IEntityRepository<TEntity>
+    where TEntity : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+
+        public NHibernateRepositoryBase(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                return filter == null
+                    ? session.Query<TEntity>().ToList()
+                    : session.Query<TEntity>().Where(filter).ToList();
+
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                return session.Query<TEntity>().FirstOrDefault(filter);
+            }
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                session.Save(entity);
+                return entity;
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                session.Update(entity);
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                session.Delete(entity);
+            }
+        }
+    }
+}
