f06b49f0be4795b1cb0c46bbc9047bda0f23f054
emrah karaman <ekaraman89@hotmail.com>
2020-05-24 00:50:47 +0300

    Veri erişim katmanı entegrasyonu classları eklendi

---
+++ Commerce.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Commerce.Core.DataAccess;
+using Commerce.Entities.Concreate;
+
+namespace Commerce.DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+    }
+}
