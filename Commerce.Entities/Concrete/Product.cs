c5c09f4232945906cdb2e3971b052973da891cdc
emrah karaman <ekaraman89@hotmail.com>
2020-06-14 13:32:36 +0300

    klasor adi degisti

---
+++ Commerce.Entities/Concrete/Product.cs
@@ -0,0 +1,13 @@
+﻿using Commerce.Core.Entities;
+
+namespace Commerce.Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public virtual int ProductId { get; set; }
+        public virtual string ProductName { get; set; }
+        public virtual int CategoryId { get; set; }
+        public virtual string QuantityPerUnit { get; set; }
+        public virtual decimal UnitPrice { get; set; }
+    }
+}
