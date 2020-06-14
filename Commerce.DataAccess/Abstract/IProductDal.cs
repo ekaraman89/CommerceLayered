79a04529942e40f955ec48c7e93e28ca31b5eafa
emrah karaman <ekaraman89@hotmail.com>
2020-06-14 13:34:26 +0300

    using klasor adi degisti

--- Commerce.DataAccess/Abstract/IProductDal.cs
+++ Commerce.DataAccess/Abstract/IProductDal.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Commerce.Core.DataAccess;
-using Commerce.Entities.Concreate;
+﻿using Commerce.Core.DataAccess;
+using Commerce.Entities.Concrete;
 
 namespace Commerce.DataAccess.Abstract
 {
